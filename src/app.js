e101f8030ecad9c3d25d6d19c29b2d87f4d5556a
Pierre-Yves Léglise <contact@axialdata.net>
2023-01-03 15:04:44 -0400

    DAJR-45, DAJR-58, finalisation et build

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 /* Inject css */
 require('./css/style.scss')
+require.context('./assets', true, /\.(png|svg|jpg|jpeg|gif|otf|cur|mp4)$/i)
+require.context('./data', true, /\.json$/i)
 const header = require('./scripts/utils/header')
 const footer = require('./scripts/utils/footer')
 header.displayHeader()
@@ -10,7 +12,11 @@ footer.displayFooter()
 const url = new URL(window.location.href)
 const _id = url.searchParams.get('id')
 const _mediaId = url.searchParams.get('mediaId')
-let calledPage = url.pathname.replace(/\//, '')
+const _sortCriteria = url.searchParams.get('sortCriteria')
+const _orderUp = url.searchParams.get('orderUp')
+
+// let calledPage = url.pathname.replace(/\//, '')
+let calledPage = url.pathname.split('/').slice(-1)[0]
 
 if (!calledPage) {
   calledPage = 'index.html'
@@ -21,7 +27,7 @@ const scriptToLoad = calledPage.replace(/.html/, '')
 
 switch (scriptToLoad) {
   case 'photographer':
-    require('./scripts/pages/photographer')(_id, _mediaId)
+    require('./scripts/pages/photographer')(_id, _mediaId, _sortCriteria, _orderUp)
     break
   default:
     require('./scripts/pages/index')
